db89a7c5ef226e2ffbdfb30973a47e51a3c0b787
Colby Chiang <cc2qe@virginia.edu>
2017-02-17 10:25:26 -0600

    time span variable for history

--- disktime.R
+++ disktime.R
@@ -13,6 +13,9 @@ c.names <- c('date', 'time', 'epoch', 'wait')
 work.dir <- '.'
 data.dir <- paste0(work.dir, '/data')
 
+# set timespan of the past 4 days
+timespan <- c(Sys.time() - as.difftime(4, unit="days"), Sys.time())
+
 # read data
 disk.data <- NULL
 for (disk.name in c('gc2718', 'gc2719', 'gc2802')) {
@@ -37,22 +40,22 @@ p <- p + geom_line()
 p <- p + scale_y_log10(breaks=10**(-1:10))
 p <- p + annotation_logticks(side='l', scaled=TRUE)
 p <- p + xlab('Date') + ylab('Wait (s)')
-p <- p + scale_x_datetime(date_breaks='12 hours', labels=date_format("%H:%M\n%m/%d", tz = "America/Chicago"))
+p <- p + scale_x_datetime(date_breaks='12 hours', labels=date_format("%H:%M\n%m/%d", tz = "America/Chicago"), limits = timespan)
+# p <- p + scale_x_datetime(date_breaks='12 hours', date_labels="%H:%M\n%m/%d", limits = timespan)
 print(p)
 
 ## ggsave(paste0(work.dir, '/plots/', 'disktime.pdf'), p, h=4.5, w=7)
-
 
 p <- ggplot(data=disk.data, aes(x=posix, y=wait, col=disk, fill=disk))
 p <- p + theme_bw() + scale_color_gdocs() + scale_fill_gdocs()
 p <- p + theme(panel.border = element_blank(), axis.text = element_text(color='black'), axis.line.x = element_line(), axis.line.y = element_line(), panel.grid=element_blank())
 ## p <- p + geom_point()
-p <- p + geom_smooth(method="loess", se=T, span=0.2, formula=y~x, alpha=0.1)
+p <- p + geom_smooth(method="loess", se=T, span=0.1, formula=y~x, alpha=0.1)
 ## p <- p + scale_y_log10(breaks=10**(-1:10))
 ## p <- p + annotation_logticks(side='l', scaled=TRUE)
 p <- p + xlab('Date') + ylab('Wait (s)')
-p <- p + scale_x_datetime(date_breaks='12 hours', labels=date_format("%H:%M\n%m/%d", tz = "America/Chicago"))
+p <- p + scale_x_datetime(date_breaks='12 hours', labels=date_format("%H:%M\n%m/%d", tz = "America/Chicago"), limits = timespan)
+# p <- p + scale_x_datetime(date_breaks='12 hours', date_labels="%H:%M\n%m/%d", limits = timespan)
 print(p)
 
 garbage <- dev.off()
-
